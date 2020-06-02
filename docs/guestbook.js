63dfc5bb0cc498078bba9a7fe2c1c31fe393ed72
ShivaniKumar98 <66299185+ShivaniKumar98@users.noreply.github.com>
2020-06-02 20:31:41 +0530

    Update guestbook.js

--- docs/guestbook.js
+++ docs/guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://20a9e6f3.eu-gb.apigw.appdomain.cloud/guestbook';
+const apiUrl = 'https://2e704414.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
